1cdd1c482601f9268c49a644901d17b2b8e63e17
aporat28 <aporat28@gmail.com>
2025-04-15 09:54:44 -0400

    mac 13 support

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "APUserAgentGenerator",
     platforms: [
-        .macOS(.v14),
+        .macOS(.v13),
         .iOS(.v15),
         .tvOS(.v13),
         .watchOS(.v6)
